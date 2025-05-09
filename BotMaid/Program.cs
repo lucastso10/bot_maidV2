ca6106f4efaaba17253e231b8d9d1c2b78b9e28e
lucastso10 <lucastso10@gmail.com>
2025-05-09 11:58:23 -0300

    initial commit

---
+++ BotMaid/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Hosting;
+
+using NetCord;
+using NetCord.Hosting.Gateway;
+using NetCord.Hosting.Services;
+using NetCord.Hosting.Services.ApplicationCommands;
+using NetCord.Rest;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+builder.Services
+    .AddDiscordGateway()
+    .AddApplicationCommands();
+
+var host = builder.Build();
+
+// Add commands using minimal APIs
+host.AddSlashCommand("ping", "Ping!", () => "Pong!")
+    .AddUserCommand("Username", (User user) => user.Username)
+    .AddMessageCommand("Length", (RestMessage message) => message.Content.Length.ToString());
+
+// Add commands from modules
+host.AddModules(typeof(Program).Assembly);
+
+// Add handlers to handle the commands
+host.UseGatewayEventHandlers();
+
+await host.RunAsync();
---
+++ README.md
@@ -0,0 +1,5 @@
+# Bot Maid V2
+
+A rewrite of [bot_maid](https://github.com/lucastso10/bot_maid) in C#.
+
+# Work in progress
